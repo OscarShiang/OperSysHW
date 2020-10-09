85c778cfa0ed905ad9a7b65e869ff1ba13286b39
Oscar Shiang <oscar0225@livemail.tw>
2020-10-09 15:05:27 +0800

    Convert the csv file into json format
    Use `strtok_r` to extract the data seperated by "|" character and
    translate the whole file into json format.
    
    The next step may be the following two:
      1. write the output file using system call directly
      2. use pthreads to implement it.

--- hw2/csv2json.c
+++ hw2/csv2json.c
@@ -1,22 +1,48 @@
 #include <pthread.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-#define IPT "input.csv"
+#define IPT "test.csv"
 #define OUT "output.json"
 
 int main(int argc, char *argv[])
 {
-    char str[] = "1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20";
+    /* Open the files */
+    FILE *in = fopen(IPT, "r");
+    FILE *out = fopen(OUT, "w");
 
     char *last = NULL, *delim = "|";
-    char *tok = strtok_r(str, delim, &last);
-    while (tok) {
-        printf("%s\n", tok);
-        tok = strtok_r(NULL, delim, &last);
-    }
+    char *postfix = ",\n";
+    bool begin = true;
 
+    char buf[300];
+    fprintf(out, "[");
+    while (1) {
+        /* Read the lines and determine when to break */
+        int ret = fscanf(in, "%s", buf) == EOF;
+        fprintf(out, "%s", &postfix[ret | begin]);
+        begin = false;
+        if (ret)
+            break;
+
+        int i = 1;
+        fprintf(out, "\t{\n");
+        char *tok = strtok_r(buf, delim, &last);
+        while (tok) {
+            fprintf(out, "\t\t\"col_%d\":%s", i, tok);
+            tok = strtok_r(NULL, delim, &last);
+
+            int flag = i++ == 20;
+            fprintf(out, "%s", &postfix[flag]);
+        }
+        fprintf(out, "\t}");
+    }
+    fprintf(out, "]\n");
+
+    fclose(in);
+    fclose(out);
     return 0;
 }

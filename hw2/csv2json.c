86fe8135ab8815f2fb93035765e90cb70049446c
Oscar Shiang <oscar0225@livemail.tw>
2020-10-17 09:07:44 +0800

    Fix error of duplicated output
    In the scenario when multiple threads are doing the tasks, when some
    threads tend to execute faster, then it may be possible to be scheduled
    to do another task.
    
    I add one more check on the scheduler to ensure the thread to be
    scheduled does not have some task in pending.

--- hw2/csv2json.c
+++ hw2/csv2json.c
@@ -10,15 +10,16 @@
 #include <time.h>
 #include <unistd.h>
 
-#define IPT "test.csv"
+#define IPT "tmp.csv"
 #define OUT "output.json"
-#define WORKER_NUM 8
+#define WORKER_NUM 3
 
 sem_t idle_thd;
 sem_t task_completed;
 
 typedef struct {
     sem_t task;
+    sem_t completed;
     bool exit;
     char input[500];
     char out[500];
@@ -27,10 +28,11 @@ typedef struct {
 /* find the index of idle thread */
 static int thd_sched(convert_args thds[])
 {
-    int index = -1, tmp;
+    int index = -1, task, completed;
     for (int i = 0; i < WORKER_NUM; i++) {
-        sem_getvalue(&thds[i].task, &tmp);
-        if (!tmp) {
+        sem_getvalue(&thds[i].task, &task);
+        sem_getvalue(&thds[i].completed, &completed);
+        if (!task && !completed) {
             index = i;
             break;
         }
@@ -56,33 +58,32 @@ void *convert_worker(void *arg)
         }
 
         int len = 0, data_cnt = 1;
-        args->out[0] = '\0';
+        args->out[0] = 0;
         strcat(args->out, "\t{\n");
-
 
         len += 3;
 
         char *tok, *last = NULL;
 
+        // printf("[worker] %s\n", args->out);
+
         tok = strtok_r(args->input, "|", &last);
         while (tok) {
             printf("[worker] tok: %s\n", tok);
-            len += snprintf(args->out + len, 500 - len, "\t\t\"col%d\":%s",
-                            data_cnt, tok);
+            len += sprintf(args->out + len, "\t\t\"col_%d\":%s", data_cnt, tok);
 
             int flag = (data_cnt++ == 20);
-            strncat(args->out, &",\n"[flag], 500 - len);
+            strcat(args->out, &",\n"[flag]);
             len += 2 - flag;
 
             tok = strtok_r(NULL, "|", &last);
         }
-        strncat(args->out, "\t}", 500 - len);
-        len += 2;
-        args->out[len] = '\0';
+        strcat(args->out, "\t}");
+
         printf("[worker] completed: %s\n", args->out);
 
         printf("[worker] task completed\n");
-        sem_post(&task_completed);
+        sem_post(&args->completed);
     }
 }
 
@@ -103,11 +104,13 @@ int main(int argc, char *argv[])
 
     for (int i = 0; i < WORKER_NUM; i++) {
         sem_init(&worker_args[i].task, 0, 0);
+        sem_init(&worker_args[i].completed, 0, 0);
         worker_args[i].exit = false;
         pthread_create(&workers[i], 0, convert_worker, &worker_args[i]);
     }
 
     char *out_buf[WORKER_NUM];
+    sem_t *completed[WORKER_NUM];
 
     fprintf(out, "[");
     while (1) {
@@ -121,9 +124,13 @@ int main(int argc, char *argv[])
                 index = thd_sched(worker_args);
             } while (index == -1);
 
+            printf("[sced] %d\n", index);
+
             ret = fscanf(in, "%s", worker_args[index].input);
             printf("[main] pass: %s\n", worker_args[index].input);
+
             out_buf[i] = worker_args[index].out;
+            completed[i] = &worker_args[index].completed;
 
             if (ret == EOF) {
                 break;
@@ -134,16 +141,16 @@ int main(int argc, char *argv[])
 
         printf("[main] i = %d\n", i);
 
+        // FIXME: this cannot prevent the threads asychronous problems
         /* wait for the worker */
         for (int j = 0; j < i; j++)
-            sem_wait(&task_completed);
+            sem_wait(completed[j]);
 
         printf("print out\n");
 
         /* Output */
         for (int j = 0; j < i; j++) {
-
-            printf("[main] print out: %s\n", out_buf[j]);
+            printf("[main] print out [%d]: \n%s\n", j, out_buf[j]);
             fprintf(out, "%s", &",\n"[begin]);
             fprintf(out, "%s", out_buf[j]);
             begin = false;

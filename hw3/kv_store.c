0a37c76a0112b861b62b864e582db23e140382c0
Oscar Shiang <oscar0225@livemail.tw>
2020-11-30 14:04:32 +0800

    Implement operations of the input
    We implement 3 different operations in this commit, that are:
    * PUT: store the key-value into the storage
    * GET: retrieve a value from the storage
    * SCAN: retrieve multiple values from the storage

--- hw3/kv_store.c
+++ hw3/kv_store.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <errno.h>
 #include <fcntl.h>
 #include <stdbool.h>
 #include <stdint.h>
@@ -10,7 +11,7 @@
 #include <unistd.h>
 
 #define BUCKET_SIZE (1 << 28) /* 256 MiB */
-#define BUCKET_MOD 0xfffffffUL
+#define BUCKET_MOD ((uint64_t) 0xfffffff)
 
 #define KEY_SIZE 129 /* 128 + null terminated */
 
@@ -38,7 +39,7 @@ int main(int argc, char *argv[])
     }
     sprintf(tmp_name, ".%s.output", ipt_name);
 
-    mkdir("store", 511);
+    mkdir("storage", 511);
 
     bool has_out = false;
     FILE *input = fopen(argv[1], "r");
@@ -47,18 +48,20 @@ int main(int argc, char *argv[])
     char cmd[5] = {0};
     uint64_t key1, key2;
     char value[129];
+    char bucket_name[512];
+
     while (fscanf(input, "%s", cmd) != EOF) {
         if (!strcmp(cmd, "PUT")) {
             fscanf(input, "%lu %s", &key1, value);
             // printf("[%llu] -> %s\n", key1, value);
 
             /* do store */
-            char bucket_name[512];
-            sprintf(bucket_name, "store/%lu.bucket", key1 >> 28);
-            int fd = open(bucket_name, O_CREAT | O_WRONLY);
-            assert(fd != -1 || "Fail to open the file");
+            sprintf(bucket_name, "storage/%lu.bucket", key1 >> 28);
+            int fd = open(bucket_name, O_CREAT | O_WRONLY, S_IRWXU);
+            assert(fd != -1);
 
             ftruncate(fd, (uint64_t) 129 * BUCKET_SIZE);
+
             uint64_t offset = key1 & BUCKET_MOD;
             lseek(fd, 129 * offset, SEEK_SET);
             write(fd, value, 129);
@@ -68,8 +71,9 @@ int main(int argc, char *argv[])
             fscanf(input, "%lu", &key1);
             // printf("get [%llu]\n", key1);
 
-            char bucket_name[512];
-            sprintf(bucket_name, "store/%lu.bucket", key1 >> 28);
+            has_out = true;
+
+            sprintf(bucket_name, "storage/%lu.bucket", key1 >> 28);
             int fd = open(bucket_name, O_RDONLY);
             if (fd == -1) {
                 // bucket not found
@@ -79,46 +83,53 @@ int main(int argc, char *argv[])
                 uint64_t offset = key1 & BUCKET_MOD;
                 lseek(fd, 129 * offset, SEEK_SET);
                 read(fd, value, 129);
-                fprintf(out, "%s\n", value);
+                if (!value[0])
+                    fprintf(out, "EMPTY\n");
+                else
+                    fprintf(out, "%s\n", value);
                 close(fd);
             }
         } else if (!strcmp(cmd, "SCAN")) {
             fscanf(input, "%lu %lu", &key1, &key2);
 
+            has_out = true;
+
             // basically use assert to check if the scan across different pages
             assert(key1 >> 28 == key2 >> 28 || "Cross the page");
 
-            uint64_t count = key2 & BUCKET_MOD - key1 & BUCKET_MOD;
+            uint64_t count = (key2 & BUCKET_MOD) - (key1 & BUCKET_MOD) + 1;
 
-            char bucket_name[512];
-            sprintf(bucket_name, "store/%lu.bucket", key1 >> 28);
+            sprintf(bucket_name, "storage/%lu.bucket", key1 >> 28);
             int fd = open(bucket_name, O_RDONLY);
             if (fd == -1) {
                 for (uint64_t i = 0; i < count; i++)
                     fprintf(out, "EMPTY\n");
             } else {
-                lseek(fd, (uint64_t) 129 * key1 & BUCKET_MOD, SEEK_SET);
+                lseek(fd, (uint64_t) 129 * (key1 & BUCKET_MOD), SEEK_SET);
                 for (uint64_t i = 0; i < count; i++) {
+                    value[0] = '\0';
                     read(fd, value, 129);
-                    fprintf(out, "%s\n", value);
-                    lseek(fd, 129, SEEK_CUR);
+                    if (!value[0])
+                        fprintf(out, "EMPTY\n");
+                    else
+                        fprintf(out, "%s\n", value);
                 }
                 close(fd);
             }
-        } else {
+        } else
             assert(0 && "Unknown command");
-        }
-    }
-
-    if (has_out) {
-        char out_name[512];
-        sprintf(out_name, "%s.output", ipt_name);
-        rename(tmp_name, out_name);
     }
 
     // close the files
     fclose(input);
     fclose(out);
+
+    if (has_out) {
+        char out_name[512];
+        sprintf(out_name, "%s.output", ipt_name);
+        rename(tmp_name, out_name);
+    } else
+        remove(tmp_name);
 
     return 0;
 }
